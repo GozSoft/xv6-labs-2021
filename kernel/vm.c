c8a850a037392a1140756ab306c33459ce0399d2
GozSoft <gaomez_@outlook.com>
2022-02-04 11:13:37 +0800

    Done print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,22 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_rec(pagetable, 1);  // 1 for root, 3 for leaf
+}
+
+void vmprint_rec(pagetable_t pagetable, int lv) {
+  if(lv == 4) return;
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) ) {
+      // this PTE points to a lower-level page table.
+      for(int j = 1; j <= lv; j++) printf(" ..");
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      uint64 child = PTE2PA(pte);
+      vmprint_rec((pagetable_t)child, lv + 1);
+    }
+  }
+}
